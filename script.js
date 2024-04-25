4e365500c10d76b73ebf95c94112ff12ab7d8450
arshjeetkaur72 <131320253+arshjeetkaur72@users.noreply.github.com>
2024-04-25 21:06:53 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var dropdowns = document.querySelectorAll('.dropdown');
 
-    dropdowns.forEach(function(dropdown) {
-        var trigger = dropdown.querySelector('a');
-        var dropdownContent = dropdown.querySelector('.dropdown-content');
-
-        trigger.addEventListener('click', function(e) {
-            e.preventDefault();
-            dropdownContent.classList.toggle('show');
-        });
-
-        window.addEventListener('click', function(e) {
-            if (!dropdown.contains(e.target)) {
-                dropdownContent.classList.remove('show');
-            }
-        });
-    });
-});
